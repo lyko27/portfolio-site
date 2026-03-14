a140e7379b4de6e8a1b9893d0ca50ea54f2365eb
lyko27 <nateogadaix@gmail.com>
2026-03-14 20:24:27 +0100

    changement de config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://perso.isima.fr',
+  base: '/~nagadaix',
   vite: {
     plugins: [tailwindcss()]
   },
